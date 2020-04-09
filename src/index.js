ea05a19accf32860541a5e973263af012f2e150c
ikey2244 <ikey2244@gmail.com>
2020-04-09 13:21:37 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,70 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+function createContext() {
+  return { prisma }
+}
+
+module.exports = {
+  createContext
+}
 
 // The GraphQL schema
 const typeDefs = gql`
   type Query {
     "A simple type for getting started!"
-    hello: String
+    users: [User]
+  }
+
+  type Mutation {
+    createUser: User
+  }
+
+  type User {
+    name: String
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    users: async (parent, args, context) => {
+      const allUsers = await context.prisma.user.findMany({
+        include: { 
+          posts: true,
+          profile: true 
+        },
+      })
+      return allUsers
+      
+    }
   },
+  Mutation: {
+    createUser: async (parent, args, context) => {
+      const user = await context.prisma.user.create({
+        data: {
+          name: "isaac@businesscreditreports.com",
+          email: "Isaac Weber",
+          posts: {
+            create: { title: "Hello World" },
+          },
+          profile: {
+            create: { bio:  "I like turtles" }
+          }
+        }
+      })
+
+      return user
+    }
+  }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: createContext,
   playground: true,
   introspection: true
 });
